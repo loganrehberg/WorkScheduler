5c78916cb9f30b51521d671b56c44c65a25d2fb2
loganrehberg <loganrehberg7@gmail.com>
2023-07-10 15:30:12 -0400

    scrapped the JS to start over from scratch. Added the color coordination based on the hour

--- assets/script.js
+++ assets/script.js
@@ -20,69 +20,21 @@
 //
 // TODO: Add code to display the current date in the header of the page.
 
- $(document).ready(function() {
-      // Display the current day at the top of the calendar
-      const currentDay = dayjs().format('dddd, MMMM D, YYYY');
-      $('#currentDay').text(currentDay);
+// Get the current date and display it at the top of the calendar
+function displayCurrentDay (){
+  var currentDayEl = document.getElementById('currentDay');
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  currentDayEl.textContent = currentDate;
+}
 
-      // An array to store events
-      let events = JSON.parse(localStorage.getItem('events')) || [];
+// set the color coding for each time block based on past, present, or furute
+function setColorCoding() {
+  var currentHour = dayjs().hour();
 
-      // Function to render the timeblocks
-      function renderTimeblocks() {
-        $('#timeblocks').empty();
-        const currentHour = dayjs().hour();
-        for (let hour = 9; hour <= 17; hour++) {
-          const time = dayjs().hour(hour).minute(0).format('h:mm A');
-          const event = events.find(e => e.hour === hour);
-          const eventText = event ? event.text : '';
-          let timeblockClass = 'timeblock';
-          if (hour < currentHour) {
-            timeblockClass += ' past';
-          } else if (hour === currentHour) {
-            timeblockClass += ' present';
-          } else {
-            timeblockClass += ' future';
-          }
-          const timeblockHTML = `<div class="${timeblockClass}" data-hour="${hour}"><div class="time">${time}</div><textarea class="event-text">${eventText}</textarea><button class="save-btn">Save</button></div>`;
-          $('#timeblocks').append(timeblockHTML);
-        }
-      }
+  //loop through each time block
+  $('.time-block').each(function(){
+    var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
 
-      // Function to save an event
-      function saveEvent(hour, text) {
-        const eventIndex = events.findIndex(e => e.hour === hour);
-        if (eventIndex !== -1) {
-          events[eventIndex].text = text;
-        } else {
-          events.push({ hour, text });
-        }
-        localStorage.setItem('events', JSON.stringify(events));
-      }
-
-      // Event listener for saving an event
-      $(document).on('click', '.save-btn', function() {
-        const hour = $(this).parent().data('hour');
-        const text = $(this).siblings('.event-text').val();
-        saveEvent(hour, text);
-      });
-
-      // Render the timeblocks
-      renderTimeblocks();
-    });
-
-  //save button for saving the data in the text area
-  $(".saveBtn").on( "click", function(event) {
-      event.preventDefault();
-      //$this is used to target the current element being clicked on and finding the parent ID name
-      var timeSlotEl = $(this).closest('.time-block').attr('id');
-      //uses name gathered in the "timeSlotEl" variable to pull the ID for the parent Element in HTML
-      var timeSlotId = document.getElementById(timeSlotEl);
-      //uses the parent element to select the text area child of the parent identified in the timeSLotID variable
-      var textSlotEl = timeSlotId.querySelector('textarea')
-      //assigns the value in the text area to the saveInput variable
-      var saveInput = textSlotEl.value;
-        //saves the value in local storage with the name of the parent element as the name and the text value as the value
-        localStorage.setItem(timeSlotEl, saveInput);
-  } );
-
+    
+  })
+}
